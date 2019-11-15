03d987037fd812fbac02cdc632ccd965b80b4b30
camilo hernandez <juaca2009@gmail.com>
2019-11-15 17:41:49 -0500

    corregido object pool

--- gestor historias clinicas/clases/atencion_pool.py
+++ gestor historias clinicas/clases/atencion_pool.py
@@ -21,17 +21,14 @@ class atencion_pool(object):
         else:
             return None
     
-    def retornar_elemento(self, elemento):
-       pass
-
     def crear_examen(self, _base, _posicion, _documento, _nombrep, _apellidop, _tipox):
         return examen(_base, _posicion, _documento, _nombrep, _apellidop, _tipox)
 
     def crear_consulta(self, _base, _posicion, _documento, _nombrep, _apellidop,
-                       _especializacion, _nombred, _apellidod, _documentod):
+                       _especializacion, _nombred, _apellidod):
 
         return consulta(_base, _posicion, _documento, _nombrep, _apellidop,
-                        _especializacion, _nombred, _apellidod, _documentod)
+                        _especializacion, _nombred, _apellidod)
 
     def organizar_atenciones(self):
         cont1 = 0
@@ -57,6 +54,21 @@ class atencion_pool(object):
             if i.posicion != None:
                 self.__atenciones.append(self.crear_examen(self.__base,i._posicion, i.nro_documento,i.nombre_paciente, i.apellido_paciente, i.tipo_examen))
         self.organizar_atenciones()
+
+    def cargar_consultas(self, _nro_cola):
+        temp = self.__base.execute(
+            """
+            select * from colas_consultas where nro_cola = %s
+            """,
+            ([_nro_cola])
+        )
+        for i in temp:
+             if i.posicion != None:
+                 self.__atenciones.append(self.crear_consulta(self.__base, i.posicion, i.nro_documento, i.nombre_paciente, i.apellido_paciente, i.especialidad, i.nombre_doctor, i.apellido_doctor))
+        self.organizar_atenciones()
+
+    def obtener_primera_posicion(self):
+        return self.__atenciones[0] 
 
 # a = gestor_bd('historias_clinicas')
 # a.conectar_bd()
