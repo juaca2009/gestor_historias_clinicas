463dfcb8555cbc9d82f433525e6c62239244ea74
camilo hernandez <juaca2009@gmail.com>
2019-11-19 11:29:27 -0500

    interfaz admin lista

--- gestor historias clinicas/app.py
+++ gestor historias clinicas/app.py
@@ -1,41 +1,135 @@
 from flask import Flask, render_template, request, redirect, session, g, url_for
-from flask_sqlalchemy import SQLAlchemy
 import os
+from clases.sistema import sistema
 
-dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = os.urandom(24)
-db = SQLAlchemy(app)
-
-class usuarios(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    correo = db.Column(db.String(50))
-    contra = db.Column(db.String(8))
-
-""" new_usuario = usuarios(correo = "juaca2009@gmail.com", contra = "12345er")
-new_usuario2 = usuarios(correo = "mimi512@gmail.com", contra = "mimi123")
-db.session.add(new_usuario2)
-db.session.add(new_usuario)
-db.session.commit() """
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = "historias_clinicas"
+sis = sistema()
 
 @app.route("/", methods = ["GET", "POST"])
-def index():
+def login():
+    global sis
     if request.method == "POST":
         session.pop('user', None)
-        user = usuarios.query.filter_by(correo = request.form["usuario"]).first()
-        if user and (user.contra == request.form["contrasena"]):
-            session['user'] = request.form["usuario"]
-            return redirect(url_for('protected'))
-    return render_template("index.html")
+        correo = str(request.form["correo"])
+        contra = str(request.form["contra"])
+        if sis.iniciar_sesion(correo, contra) == 1:
+            session['user'] = request.form["correo"]
+            return redirect(url_for('admin_m'))
+        elif sis.iniciar_sesion(correo, contra) == 2:
+            session['user'] = request.form["correo"]
+        elif sis.iniciar_sesion(correo, contra) == 3:
+            session['user'] = request.form["correo"]
+        elif sis.iniciar_sesion(correo, contra) == 4:
+            session['user'] = request.form["correo"]
+        elif sis.iniciar_sesion(correo, contra) == 5:
+            session['user'] = request.form["correo"]
+        elif sis.iniciar_sesion(correo, contra) == 6:
+            session['user'] = request.form["correo"]
+        else:
+            return redirect(url_for('login'))
+    return render_template("login.html")
 
-@app.route("/protected")
-def protected():
+
+
+
+
+
+
+
+
+
+
+#rutas admin
+@app.route("/admin")
+def admin_m():
     if g.user:
-        return render_template("protected.html")
-    return redirect(url_for('index'))
+        return render_template("admin.html")
+    return redirect(url_for('login'))
 
+@app.route("/paciente", methods = ["POST"])
+def paciente():
+    global sis
+    if g.user:
+        if request.method == "POST":
+            return redirect(url_for('reg_paciente'))
+
+@app.route("/recepcionista", methods = ["POST"])
+def recepcionista():
+    global sis
+    if g.user:
+        return redirect(url_for('reg_recepcionista'))
+        
+
+@app.route("/enfermero", methods = ["POST"])
+def enfermero():
+    global sis 
+    if g.user:
+        return redirect(url_for('reg_enfermero'))
+        
+
+@app.route("/doctor", methods = ["POST"])
+def doctor():
+    global sis 
+    if g.user:
+        return redirect(url_for('reg_doctor'))
+        
+    
+@app.route("/empresa", methods = ["POST"])
+def empresa():
+    global sis 
+    if g.user:
+        return redirect(url_for('reg_empresa'))
+
+
+@app.route("/reg_recepcionista", methods = ["GET", "POST"])
+def reg_recepcionista():
+    global sis
+    if g.user:
+        return render_template("R_recepcionista.html")
+
+@app.route("/reg_enfermero", methods = ["GET", "POST"])
+def reg_enfermero():
+    global sis
+    if g.user:
+        return render_template("R_enfermero.html")
+
+@app.route("/reg_doctor", methods = ["GET", "POST"])
+def reg_doctor():
+    global sis
+    if g.user:
+        return render_template("R_doctor.html")
+
+@app.route("/reg_paciente", methods = ["GET", "POST"])
+def reg_paciente():
+    global sis
+    if g.user:
+        return render_template("R_paciente.html")
+
+@app.route("/reg_empresa", methods = ["GET", "POST"])
+def reg_empresa():
+    global sis
+    if g.user:
+        return render_template("R_empresa.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#generales
 @app.before_request
 def before_request():
     g.user = None
@@ -45,8 +139,7 @@ def before_request():
 @app.route("/drop")
 def drop():
     session.pop('user', None)
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
