60e5da019fa069790765a49f355a81c9d7126056
camilo hernandez <juaca2009@gmail.com>
2019-11-20 07:02:48 -0500

    corregido

--- gestor historias clinicas/clases/paciente.py
+++ gestor historias clinicas/clases/paciente.py
@@ -6,33 +6,25 @@ class paciente(usuario):
     def __init__(self, _correo, _contra, _base, _nombre, _apellido, _fechan, _ciudad, _direccion, _documento):
          usuario.__init__(self, _correo, _contra, _base, _nombre, _apellido, _fechan, _ciudad, _direccion, _documento)
 
-    def obtener_historia(self):
-        temp = self.__base.execute(
+    def obtener_historia_paciente(self):
+        lis = list()
+        lis2 = list()
+        temp = temp = usuario.get_base(self).execute(
             """
             select id_historia, apellido_doctor, apellido_enfermero, 
             comentarios, especialidad, fecha, nombre_doctor, 
             nombre_enfermero, tipo_examen from paciente_historia
             where nro_documento = %s
             """,
-            ([usuario.get_documento(self)])
+            ([self.get_documento()])
         )
         for i in temp:
-            historia_clinica = 'nro historia:' + ' ' + str(i.id_historia)
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'nombre doctor:' + ' ' + i.nombre_doctor
-            historia_clinica = self.__historia_clinica + " /n"
-            historia_clinica = self.__historia_clinica + 'apellido doctor:' + ' ' + i.apellido_doctor
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'especialidad:' + ' ' + i.especialidad
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'nombre enfermero:' + ' ' + str(i.nombre_enfermero)
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'apellido enfermero:' + ' ' + str(i.apellido_enfermero)
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'fecha:' + ' ' + str(i.fecha)
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'tipo examen:' + ' ' + str(i.tipo_examen)
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + 'comentarios/resultados:' + ' ' + str(i.comentarios) 
-            historia_clinica = self.__historia_clinica + '/n'
-            historia_clinica = self.__historia_clinica + '====================================================================================================================='
+            lis.append(i.id_historia)
+            lis.append(i.fecha)
+            lis.append(i.especialidad)
+            lis.append(i.tipo_examen)
+            lis.append(i.comentarios)
+            lis2.append(lis)
+            lis = list()
+        return lis2
+            
